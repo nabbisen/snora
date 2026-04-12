15a43ec88e3de1d1d0d0ea1c762c3b6e68125003
nabbisen <nabbisen@scqr.net>
2026-04-12 18:59:54 +0900

    example app misc defs

--- examples/hei_snora/src/app.rs
+++ examples/hei_snora/src/app.rs
@@ -1,65 +1,15 @@
 use iced::Task;
 use snora::LayoutDirection;
-use strum_macros::Display;
 
 mod log;
 mod message;
+mod misc;
 mod update;
 mod view;
 
 use crate::app::log::LogEntry;
 use message::Message;
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum ViewId {
-    Home,
-    Search,
-    Settings,
-}
-
-impl std::fmt::Display for ViewId {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            ViewId::Home => write!(f, "Home"),
-            ViewId::Search => write!(f, "Search"),
-            ViewId::Settings => write!(f, "Settings"),
-        }
-    }
-}
-
-#[derive(Clone, Debug, PartialEq, Display)]
-#[strum(serialize_all = "PascalCase")]
-pub enum MenuId {
-    File,
-    Settings,
-}
-
-#[derive(Clone, Debug, PartialEq)]
-pub enum MenuItemId {
-    File(FileMenuItemId),
-    Settings(SettingsMenuItemId),
-}
-
-impl std::fmt::Display for MenuItemId {
-    fn fmt(&self, w: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            MenuItemId::File(x) => write!(w, "{}", x),
-            MenuItemId::Settings(x) => write!(w, "{}", x),
-        }
-    }
-}
-
-#[derive(Clone, Debug, PartialEq, Display)]
-#[strum(serialize_all = "PascalCase")]
-pub enum FileMenuItemId {
-    New,
-}
-
-#[derive(Clone, Debug, PartialEq, Display)]
-#[strum(serialize_all = "PascalCase")]
-pub enum SettingsMenuItemId {
-    About,
-}
+use misc::*;
 
 pub struct App {
     direction: LayoutDirection,
@@ -67,6 +17,7 @@ pub struct App {
     logs: Vec<LogEntry>,
     active_menu_id: Option<MenuId>,
     is_bottom_sheet_open: bool,
+    // context_menu_state: Option<(Point, String)>,
 }
 
 impl App {
